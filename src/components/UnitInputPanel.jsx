ca80da6cd0797c1873165440403571e1960ba0c4
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-03-04 11:07:20 +0100

    added positioning with css

--- src/components/UnitInputPanel.jsx
+++ src/components/UnitInputPanel.jsx
@@ -1,7 +1,28 @@
+import "./UnitInputPanel.css"
+
 export default function ({ type }) {
 
+    let units = ["kg", "lbs"]
+
+    if (type === "height") {
+        units = ["cm", "feet"]
+    }
+
     return (
-        <p>{type}</p>
+        <div className="input-with-heading">
+            <div >
+                <h1>{type}</h1>
+                <input type="number"></input>
+            </div>
+            <div >
+                <h1>Unit:</h1>
+                <select></select>
+            </div>
+
+        </div>
+
+
+
     );
 
 }
